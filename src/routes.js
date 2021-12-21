a2c1b5754bf9f0144c7dface34b6a320a21cab94
IkoAfianando <ikoafianando123@gmail.com>
2021-12-21 13:59:00 +0700

    Push

--- src/routes.js
+++ src/routes.js
@@ -1,31 +1,34 @@
-const {addNoteHandler, getAllNotesHandler, getNodeByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler} = require("./handler");
-const routes = [
-    {
-        method: 'POST',
-        path: '/notes',
-        handler: addNoteHandler,
-    },
-    {
-        method: 'GET',
-        path: '/notes',
-        handler: getAllNotesHandler,
-    },
-    {
-        method: 'GET',
-        path: '/notes/{id}',
-        handler: getNodeByIdHandler
-    },
-    {
-        method: 'PUT',
-        path: '/notes/{id}',
-        handler: editNoteByIdHandler
-    },
-    {
-        method: 'DELETE',
-        path: '/notes/{id}',
-        handler: deleteNoteByIdHandler
+const {
+  addNoteHandler, getAllNotesHandler, getNodeByIdHandler, editNoteByIdHandler, deleteByIdHandler,
+} = require('./handler');
 
-    }
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNodeByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteByIdHandler,
+
+  },
 ];
 
 module.exports = routes;
