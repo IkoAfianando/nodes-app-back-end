61557463f895129feb48a8a19378fa0fc0e9fe62
IkoAfianando <ikoafianando123@gmail.com>
2022-03-01 18:20:41 +0700

    create migration

--- src/api/notes/index.js
+++ src/api/notes/index.js
@@ -1,12 +1,11 @@
-const {server} = require("@hapi/hapi");
 const NotesHandler = require("./handler");
 const routes = require("./routes");
 
 module.exports = {
-    name: 'notes',
-    version: '1.0.0',
-    register: async (server, {service, validator}) => {
-        const notesHandler = new NotesHandler(service, validator);
-        server.route(routes(notesHandler));
-    }
+  name: "notes",
+  version: "1.0.0",
+  register: async (server, { service, validator }) => {
+    const notesHandler = new NotesHandler(service, validator);
+    server.route(routes(notesHandler));
+  },
 };
